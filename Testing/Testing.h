41648ac00914d36e66c88522f9debda11b77311d
Ash Furrow <ash@500px.com>
2012-08-27 19:29:26 -0400

    Added unit tests and oauth submodule.

---
+++ Testing/Testing.h
@@ -0,0 +1,13 @@
+//
+//  Testing.h
+//  Testing
+//
+//  Created by Ash Furrow on 2012-08-27.
+//  Copyright (c) 2012 AF. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Testing : SenTestCase
+
+@end
---
+++ Testing/Testing.m
@@ -0,0 +1,32 @@
+//
+//  Testing.m
+//  Testing
+//
+//  Created by Ash Furrow on 2012-08-27.
+//  Copyright (c) 2012 AF. All rights reserved.
+//
+
+#import "Testing.h"
+
+@implementation Testing
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Testing");
+}
+
+@end
